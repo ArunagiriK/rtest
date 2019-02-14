760bc039f3d609c4f658369eaa394b6bc72c8a30
stephane <smouesca@auguria.net>
2019-02-14 08:34:41 +0100

    Init Odoo module

---
+++ github/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'GitHub projects module',
+    'version': '12.0.0.3',
+    'summary': '',
+    'description': """ """,
+    'license': 'OPL-1',
+    'author': 'Auguria',
+    'website': 'http://www.auguria.com',
+
+    'depends': [
+        'base',
+        'project',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    "images": [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
